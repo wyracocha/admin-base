b55bcbbfd5ffad56827fc44651a7b70ab188553f
Miguel Aliaga <miguel@uorboros.com>
2018-01-23 10:06:26 -0500

    adding admin default credentials in config.json

--- app.js
+++ app.js
@@ -13,65 +13,82 @@ var helmet = require('helmet')
 var users = require('./routes/users');
 var auth = require('./routes/auth');
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-// hbs config
-app.engine('.hbs', exphbs({
-  extname: '.hbs',
-  layoutsDir: 'views/layouts',
-  partialsDir: 'views/partials',
-  defaultLayout: 'layout'
-}));
-app.set('view engine', '.hbs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(lessMiddleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
-// - cookie config
-var sess = {
-  secret: 'keyboard cat',
-  cookie: {
-    httpOnly: true
-  },
-  saveUninitialized: false,
-  resave: false
+async function Setup () {
+  try {
+    // view engine setup
+    app.set('views', path.join(__dirname, 'views'));
+    // hbs config
+    app.engine('.hbs', exphbs({
+      extname: '.hbs',
+      layoutsDir: 'views/layouts',
+      partialsDir: 'views/partials',
+      defaultLayout: 'layout'
+    }));
+    app.set('view engine', '.hbs');
+    // uncomment after placing your favicon in /public
+    //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(lessMiddleware(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, 'public')));
+    // - cookie config
+    var sess = {
+      secret: process.env.SECRET,
+      cookie: {
+        httpOnly: true
+      },
+      saveUninitialized: false,
+      resave: false
+    }
+    if (app.get('env') === 'production') {
+      app.set('trust proxy', 1) // trust first proxy
+      sess.cookie.secure = true // serve secure cookies
+    }
+    app.use(session(sess))
+    // - end cookie config
+    // - csrf
+    app.use(csrf({ cookie: true }));
+    // - end csrf
+    app.use('/users', users);
+    app.use('/', auth)
+    // catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+      var err = new Error('Not Found');
+      err.status = 404;
+      next(err);
+    });
+    // adding helment
+    app.use(helmet({
+      frameguard: {action: 'deny'}
+    }))
+    // error handler
+    app.use(function(err, req, res, next) {
+      // set locals, only providing error in development
+      res.locals.message = err.message;
+      res.locals.error = req.app.get('env') === 'development' ? err : {};
+      // render the error page
+      res.status(err.status || 500);
+      res.render('error');
+    });
+    return new Promise((resolve, reject) => {
+      resolve()
+    })
+  } catch (e) {
+    return new Promise((resolve, reject) => {
+      reject(new Error(e.message))
+    })
+  }
 }
-if (app.get('env') === 'production') {
-  app.set('trust proxy', 1) // trust first proxy
-  sess.cookie.secure = true // serve secure cookies
+async function Gurure() {
+  try {
+    await Setup()
+  } catch (e) {
+    return new Promise((resolve, reject) => {
+      reject(new Error(e.message))
+    })
+  }
 }
-app.use(session(sess))
-// - end cookie config
-// - csrf
-app.use(csrf({ cookie: true }));
-// - end csrf
-app.use('/users', users);
-app.use('/', auth)
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-// adding helment
-app.use(helmet({
-  frameguard: {action: 'deny'}
-}))
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
+Gurure()
 module.exports = app;
--- routes/auth.js
+++ routes/auth.js
@@ -12,10 +12,16 @@ router.get('/',  (req, res, next) => {
 router.get('/login', (req, res, next) => {
   req.session.authenticated ? res.redirect('/users') : res.render('auth/login', { csrfToken: req.csrfToken() })
 })
-router.post('/login',  (req, res, next) => {
-  req.session.name = req.body.name || 'pepe'
-  req.session.authenticated = true
-  res.redirect('/users')
+router.post('/login', (req, res, next) => {
+  if (req.body.email === process.env.ADMIN_EMAIL && req.body.password === process.env.ADMIN_PASSWORD) {
+    req.session.authenticated = true
+    res.redirect('/users')
+  } else {
+    res.render('auth/login', { 
+      csrfToken: req.csrfToken(),
+      message: 'invalid user or password'
+    })
+  }
 })
 router.get('/logout', isAuthenticated, (req, res, next) => {
   req.session.destroy(function(err) {
